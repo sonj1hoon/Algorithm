5376bee295b3377ddd00d44a35bc7db210af6269
sonjihoon <141906696+sonj1hoon@users.noreply.github.com>
2023-11-03 17:01:46 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 256 ms, Memory: 21644 KB -BaekjoonHub

---
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		//int n = Integer.parseInt(br.readLine());
+		String s = br.readLine();
+		Stack<Character> stack = new Stack<>();
+//		StringTokenizer st = new StringTokenizer(s," <>");
+//		st.hasMoreTokens();
+//		st.nextToken();
+		
+		for(int i=0; i<s.length(); i++) {
+			if(s.charAt(i)=='<'){
+                while(s.charAt(i)!='>'){
+                    sb.append(s.charAt(i));
+                    i++;
+                }sb.append('>');
+            }
+            else if(s.charAt(i)==' '){
+                sb.append(" ");
+            }
+			else{
+                while(i<s.length() && s.charAt(i) != ' ' && s.charAt(i) != '<'){
+                    stack.push(s.charAt(i));
+                    i++;
+                }
+                while(stack.empty()==false) {
+                	sb.append(stack.pop());
+                }
+                i--;
+            }
+		}
+		System.out.println(sb.toString());
+	}
+}
