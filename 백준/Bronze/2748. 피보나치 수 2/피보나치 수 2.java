2e1455a893c78e0b8d3b5d59e7165dbf7ffd6fe3
sonjihoon <141906696+sonj1hoon@users.noreply.github.com>
2024-03-12 00:05:17 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 128 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main {
+	public static long[] arr;
+    
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		arr = new long[n+1];
+        
+		for(int i=0; i<=n; i++) {
+			arr[i]=-1;
+		}
+		arr[0]=0;
+		arr[1]=1;
+		System.out.println(fib(n));
+	}
+	public static long fib(int n) {
+		if(arr[n]==-1) {
+			arr[n]=fib(n-1)+fib(n-2);
+		}
+		return arr[n];
+	}
+}
