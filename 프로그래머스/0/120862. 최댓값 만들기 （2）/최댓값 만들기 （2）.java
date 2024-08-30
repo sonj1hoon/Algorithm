a636f9d0b60eccb4977b10a57d64f7616b118d17
sonjihoon <141906696+sonj1hoon@users.noreply.github.com>
2024-08-31 06:14:15 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 1.00 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] numbers) {
+        Arrays.sort(numbers);
+        return Math.max(numbers[0]*numbers[1],numbers[numbers.length-2]*numbers[numbers.length-1]);
+    }
+}
