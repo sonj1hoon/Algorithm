9bf4f615fc3b43f57ecf79a98181c1686cded7f7
sonjihoon <141906696+sonj1hoon@users.noreply.github.com>
2024-09-06 01:13:07 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.19 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        String[] sArr = s.split(" ");
+        for(int i=0; i<sArr.length; i++){
+            if(sArr[i].equals("Z")){
+                answer-=Integer.parseInt(sArr[i-1]);
+                continue;
+            }
+            answer+=Integer.parseInt(sArr[i]);
+        }
+        return answer;
+    }
+}
