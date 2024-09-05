b442dca2aaa4b9ca8b526c80db4db990ab5996fc
sonjihoon <141906696+sonj1hoon@users.noreply.github.com>
2024-09-06 05:36:54 +0900

    [level 0] Title: 이진수 더하기, Time: 10.77 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.java
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.java
@@ -1,10 +1,28 @@
-//import java.util.*;
+import java.util.*;
 class Solution {
     public String solution(String bin1, String bin2) {
         String answer = "";
-        int n1 = Integer.parseInt(bin1,2);
-        int n2 = Integer.parseInt(bin2,2);
-        answer = Integer.toBinaryString(n1+n2);
-        return answer;
+        String[] n1 = bin1.split("");
+        String[] n2 = bin2.split("");
+        int result = 0;
+        int power = 1;
+
+        for(int i = n1.length - 1; i >= 0; i--) {
+            result += Integer.parseInt(n1[i]) * power;
+            power *= 2;
+        }
+        
+        power = 1; 
+        for(int i = n2.length - 1; i >= 0; i--) {
+            result += Integer.parseInt(n2[i]) * power;
+            power *= 2;
+        }
+
+        while(result > 0) {
+            answer = (result % 2) + answer; 
+            result /= 2;
+        }
+
+        return answer.equals("") ? "0" : answer;
     }
-}   
+}
