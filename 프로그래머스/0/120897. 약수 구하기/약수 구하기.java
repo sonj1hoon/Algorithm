e0a4c0b945dd1a99dc7f8279d51d347b820aad40
sonjihoon <141906696+sonj1hoon@users.noreply.github.com>
2024-09-09 05:13:55 +0900

    [level 0] Title: 약수 구하기, Time: 0.17 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -1,14 +1,17 @@
 import java.util.*;
 class Solution {
     public int[] solution(int n) {
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        for(int i=1; i<=n; i++)
-            if(n%i==0) list.add(i);
-        
-        int[] result = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            result[i] = list.get(i);  // 각 요소를 배열로 복사
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=1; i<=n; i++){
+            if(n%i==0){
+                list.add(i);
+            }    
         }
-        return result;
+        int[] arr = new int[list.size()];
+        for(int i=0; i<arr.length; i++){
+            arr[i] = list.get(i);
+        }
+        
+        return arr;
     }
 }
