1c2617405b0569511cad562bc8b747178b2bca4c
sonjihoon <141906696+sonj1hoon@users.noreply.github.com>
2024-09-26 01:24:26 +0900

    [level 0] Title: 카운트 다운, Time: 0.03 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -2,8 +2,7 @@ class Solution {
     public int[] solution(int start_num, int end_num) {
         int[] answer = new int[start_num-end_num+1];
         for(int i=0; i<answer.length; i++){
-            answer[i]=start_num;
-            start_num--;
+            answer[i]=start_num-i;
         }
         return answer;
     }
