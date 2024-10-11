5bc1e7794094322e805568f843c5180dab589ea3
sonjihoon <141906696+sonj1hoon@users.noreply.github.com>
2024-10-12 04:13:01 +0900

    [level 2] Title: 주식가격, Time: 35.53 ms, Memory: 88 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int i=0; i<prices.length; i++){
+            while(!stack.isEmpty() && prices[stack.peek()] > prices[i]){
+                int idx = stack.pop();
+                answer[idx] = i-idx;
+            }stack.push(i);
+        }
+        while(!stack.isEmpty()){
+            int idx = stack.pop();
+            answer[idx] = prices.length-1-idx;
+        }
+        return answer;
+    }
+}
