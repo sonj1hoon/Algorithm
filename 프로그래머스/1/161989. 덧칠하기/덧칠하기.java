f51996ece799a8fbc1b83d07c6046ee5f0053070
sonjihoon <141906696+sonj1hoon@users.noreply.github.com>
2025-01-18 18:09:21 +0900

    [level 1] Title: 덧칠하기, Time: 2.40 ms, Memory: 83.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 1;
+        int start = section[0]; 
+        int end = section[0] + (m-1);
+        
+        for(int i:section){
+            if(i>=start && i<=end){
+                continue;
+            }else{
+                start = i;
+                end = i + (m-1);
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
