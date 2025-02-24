badedb228b3a06e4dabe48c81db40de635900969
sonjihoon <141906696+sonj1hoon@users.noreply.github.com>
2025-02-24 16:49:12 +0900

    [level 1] Title: 숫자 짝꿍, Time: 89.98 ms, Memory: 148 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -1,26 +1,26 @@
 class Solution {
     public String solution(String X, String Y) {        
-        int[] intX = new int[10];
-        int[] intY = new int[10];
+        int[] countX = new int[10];
+        int[] countY = new int[10];
         
-        for (char c : X.toCharArray()) {
-            intX[c - '0']++;
+        for(char c : X.toCharArray()){
+            countX[c-'0']++;
         }
-
-        for (char c : Y.toCharArray()) {
-            intY[c - '0']++;
+        for(char c : Y.toCharArray()){
+            countY[c-'0']++;
         }
         
         StringBuilder sb = new StringBuilder();
-        for (int i = 9; i >= 0; i--) {
-            int pairCount = Math.min(intX[i], intY[i]);
-            for (int j = 0; j < pairCount; j++) {
+        
+        for(int i=9; i>=0; i--){
+            int pairCnt = Math.min(countX[i], countY[i]);
+            for(int j=0; j<pairCnt; j++){
                 sb.append(i);
             }
         }
         
-        if (sb.length() == 0) return "-1";
-        if (sb.toString().charAt(0) == '0') return "0";
+        if(sb.length()==0) return "-1";
+        if(sb.toString().charAt(0)=='0') return "0";
         
         return sb.toString();
     }
