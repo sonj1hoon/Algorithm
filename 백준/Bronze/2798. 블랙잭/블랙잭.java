b5c6d644c70f9b151bfc95c07ec93510d7873aaa
sonjihoon <141906696+sonj1hoon@users.noreply.github.com>
2025-02-25 19:35:13 +0900

    [Bronze II] Title: 블랙잭, Time: 104 ms, Memory: 14024 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,37 +1,35 @@
 import java.io.*;
 import java.util.*;
-public class Main{
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int[]c=new int[N];		
-		StringTokenizer st1 = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) {
-			c[i] = Integer.parseInt(st1.nextToken());
-		}
-		
-		int result = 0;
-		int compare = 0;
-		for(int i=0; i<N-2; i++) {
-			for(int j=i+1; j<N-1; j++) {
-				for(int k=j+1; k<N; k++) {
-					result = c[i]+c[j]+c[k];
-					
-					if(result==M) {
-						System.out.println(result);
-						i=N-3;
-						j=N-2;
-						break;
-					}
-					if(compare<result&&result<M) {
-						compare=result;
-					}
-				}
-			}
-		}if(result!=M) {
-			System.out.println(compare);
-		}
-	}
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st1.nextToken());
+        int M = Integer.parseInt(st1.nextToken());
+        int[] arr = new int[N];
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(st2.nextToken());
+        }
+        System.out.println(cal(arr,N,M));
+    }
+    static int cal(int[] arr, int N, int M){
+        int compare=0;
+        for(int i=0; i<N-2; i++) {
+            for (int j = i + 1; j < N - 1; j++) {
+                for (int k = j + 1; k < N; k++) {
+                    int result = arr[i] + arr[j] + arr[k];
+
+                    if (result == M) {
+                        return result;
+                    }
+                    if (result < M && result > compare) {
+                        compare = result;
+                    }
+                }
+            }
+        }return compare;
+    }
 }
