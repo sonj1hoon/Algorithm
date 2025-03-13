b7f0601177f8bb10464316de8440981f535e58fd
sonjihoon <141906696+sonj1hoon@users.noreply.github.com>
2025-03-13 14:29:02 +0900

    [Silver IV] Title: 카드2, Time: 164 ms, Memory: 45460 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Queue<Integer> q = new LinkedList<>();
+
+        int N = Integer.parseInt(br.readLine());
+        for(int i=1; i<=N; i++){
+            q.add(i);
+        }
+        while(q.size()>1){
+            q.poll();
+            q.offer(q.poll());
+        }
+        System.out.println(q.poll());
+    }
+}
